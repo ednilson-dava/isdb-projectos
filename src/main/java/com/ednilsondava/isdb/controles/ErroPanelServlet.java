76d98b899b5c5e746dd0a8f7d5d8184ec4d485ef
Ednilson <dava.ednilson@gmail.com>
2020-05-05 16:03:37 +0200

    Mostrar a pagina de Diagnostico de erro passa a exibir os detalhes dos erros;

--- src/main/java/com/ednilsondava/isdb/controles/ErroPanelServlet.java
+++ src/main/java/com/ednilsondava/isdb/controles/ErroPanelServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @WebServlet("/ErroPanel")
 public class ErroPanelServlet extends HttpServlet {
@@ -28,6 +31,26 @@ public class ErroPanelServlet extends HttpServlet {
         String requestUri = (String) req.getAttribute("javax.servlet.error.request_uri");
         if(requestUri == null) requestUri = "Unknown";
 
+        DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss");
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("@").append(dateFormat.format(Calendar.getInstance().getTime())).append("@").append("<br/>");
+        builder.append("@"+message+"@").append("<br/>");
+        builder.append("@"+servletName+"@").append("<br/>");
+        builder.append("@"+requestUri+"@").append("<br/>");
+
+        if(throwable != null) {
+            builder.append("===================================================================").append("<br/>");
+            for (StackTraceElement element : throwable.getStackTrace()) {
+                String pck = element.getClassName();
+                if(pck.startsWith("com.ednilsondava.isdb")) {
+                    String b = element.getFileName() + "(" + element.getMethodName() + ":" + element.getLineNumber() + ")";
+                    builder.append(b).append("<br/>");
+                }
+            }
+        }
+
+        req.setAttribute("stack", builder.toString());
         req.getRequestDispatcher("/WEB-INF/pages/500.jsp").forward(req, resp);
         req.logout();
     }
